0e04dc022f0ae73729a0c105ab4a0e052689ceca
moonjinho99 <anszl789@naver.com>
2025-10-25 15:15:51 +0900

    feat(Car) : Car 클래스 생성, 이름 세팅 및 유효성 검사

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String[] carNames = Console.readLine().split(",");
+        List<Car> carList = new ArrayList<>();
+
+        //1. 자동차 이름 세팅
+        for(String name : carNames) {
+
+            if(name.length() > 5)
+            {
+                throw new IllegalArgumentException("자동차의 이름은 5글자 이하만 가능합니다.");
+            }
+
+            Car car = new Car(name);
+            carList.add(car);
+        }
+
+
     }
 }
